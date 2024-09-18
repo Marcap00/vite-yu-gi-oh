ae369d3fb51ed1723a0408a5156fd867dd0d6c63
Marcap00 <cappellatomarco@yahoo.com>
2024-09-18 16:38:55 +0200

    popoluted cards with a call api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,78 @@
 <script>
+import MainCard from './MainCard.vue'
+import axios from 'axios'
+
 export default {
     data() {
         return {
-
+            urlCardInfo: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=100&offset=0',
+            cards: [],
         }
     },
+    methods: {
+        getCardsInfo() {
+            axios.get(this.urlCardInfo)
+                .then(response => {
+                    console.log(response.data.data);
+                    this.cards = response.data.data;
+                    console.log(this.cards);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
+    },
+    components: {
+        MainCard
+    },
+    mounted() {
+        this.getCardsInfo()
+    }
+
 }
 
 </script>
 
 <template>
-    <h2>Main</h2>
+    <main>
+        <div class="container">
+            <div class="wrapper">
+                <div class="banner-found-cards">
+                    <p>Found {{ this.cards.length }} Cards</p>
+                </div>
+                <div class="row row-cols">
+                    <MainCard v-for="card in cards" :key="card.id" :cardObj="card" />
+                </div>
+            </div>
+        </div>
+    </main>
 
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+
+main {
+    background-color: $bg-main;
+    min-height: 100vh;
+
+    .container {
+        padding-top: 3rem;
+
+        .wrapper {
+            background-color: #fff;
+            width: 100%;
+            min-height: 100vh;
+            padding: 1rem;
+
+            .banner-found-cards {
+                background-color: #212429;
+                color: white;
+                font-weight: 700;
+                padding: 1rem;
+            }
+        }
+    }
+
+}
+</style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        cardObj: {
+            type: Object,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="col">
+        <div class="card">
+            <img :src="cardObj.card_images[0].image_url" class="card-img-top" alt="Card Image">
+            <div class="card-body">
+                <h5 class="card-title">Card Name: <br> {{ cardObj.name }}</h5>
+                <p class="card-type">Archetype: <br> {{ cardObj.archetype }}</p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped></style>
