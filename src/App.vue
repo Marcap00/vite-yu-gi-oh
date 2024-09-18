005ef7e9b2d2e5d6bc7e24f36ac10180c4ac2c49
Marcap00 <cappellatomarco@yahoo.com>
2024-09-18 15:03:32 +0200

    imported main and header components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
 export default {
   data() {
     return {
-      message: 'Hello World!',
+
     }
   },
+  components: {
+    AppHeader,
+    AppMain
+  }
 }
 
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <div>
+    <AppHeader />
+    <AppMain />
+  </div>
 
 </template>
 
