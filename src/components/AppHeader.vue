be6addcbd0bca395a0afd71e7ccfccfa6c40d0a9
Marcap00 <cappellatomarco@yahoo.com>
2024-09-19 18:00:56 +0200

    added favicon and logo header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,16 +11,27 @@ export default {
 
 <template>
     <header>
+        <img src="../../public/logo.png" alt="Logo Yu-Gi-Oh">
         <h1>Yu-Gi-Oh API</h1>
     </header>
 </template>
 
 <style lang="scss" scoped>
 header {
+    height: 100px;
     padding: 1rem;
+    display: flex;
+    align-items: center;
+
+    img {
+        max-height: 100%;
+        width: auto;
+        margin-right: 1rem;
+
+    }
 
     h1 {
-        font-weight: 600;
+        font-weight: 500;
     }
 }
 </style>
